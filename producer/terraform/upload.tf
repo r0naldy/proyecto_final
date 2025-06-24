53628c444126bb5e36d287564954bc0b868ed55a
r0naldy <ronaldyitolopez@gmail.com>
2025-06-24 18:03:17 -0500

    🧹 Eliminar duplicado de csv_upload en upload.tf

--- producer/terraform/upload.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_object" "csv_upload" {
-  bucket        = var.bucket_name
-  key           = "raw/sales_data_dirty_20_errores.csv"
-  source        = "${path.module}/sales_data_dirty_20_errores.csv"
-  etag          = filemd5("${path.module}/sales_data_dirty_20_errores.csv")
-  content_type  = "text/csv"
-}
