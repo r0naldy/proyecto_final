7d23b0a0b73d1bad25fb739471cfb0749ac36c06
r0naldy <ronaldyitolopez@gmail.com>
2025-06-24 19:57:02 -0500

    Empaquetar y subir nueva versión de Lambda

--- producer/terraform/main.tf
+++ producer/terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Reutilizar un rol ya existente (NO lo crea)
+# Reutilizar un rol ya existente (NO se crea)
 data "aws_iam_role" "lambda_exec_role" {
   name = "lambda_csv_exec_role"
 }
@@ -18,6 +18,7 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Función Lambda con código empaquetado
 resource "aws_lambda_function" "csv_corrector" {
   function_name    = "csv_corrector_lambda"
   role             = data.aws_iam_role.lambda_exec_role.arn
@@ -26,8 +27,23 @@ resource "aws_lambda_function" "csv_corrector" {
   filename         = "${path.module}/lambda.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda.zip")
   timeout          = 3
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_s3,
+    aws_iam_role_policy_attachment.lambda_logs
+  ]
 }
 
+# Permitir que S3 invoque la función Lambda
+resource "aws_lambda_permission" "s3_invoke_permission" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.csv_corrector.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::x-bucket-cloud"
+}
+
+# Configuración de eventos S3 que disparan la Lambda
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = "x-bucket-cloud"
 
@@ -41,14 +57,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   depends_on = [aws_lambda_permission.s3_invoke_permission]
 }
 
-resource "aws_lambda_permission" "s3_invoke_permission" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.csv_corrector.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::x-bucket-cloud"
-}
-
+# Subir archivo CSV
 resource "aws_s3_object" "csv_upload" {
   bucket       = "x-bucket-cloud"
   key          = "raw/sales_data_dirty_20_errores.csv"
@@ -56,6 +65,7 @@ resource "aws_s3_object" "csv_upload" {
   content_type = "text/csv"
 }
 
+# Carpetas vacías para estructura del bucket
 resource "aws_s3_object" "raw_folder" {
   bucket = "x-bucket-cloud"
   key    = "raw/"
